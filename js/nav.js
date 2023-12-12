87883d95ee42895ac068f1c89044fb532471dcfe
Geek-ASR <132185513+Geek-ASR@users.noreply.github.com>
2023-12-12 23:38:04 +0530

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,5 @@
+const navElementConatiner = document.querySelector(".nav-container");
+const navEle = `<h3>Streamlen</h3>
+			<a href="https://github.com/streamlen/sortingAlgorithmVisualizer" target="_blank" rel="noopener noreferrer">Github</a>`;
+
+navElementConatiner.innerHTML = navEle;
